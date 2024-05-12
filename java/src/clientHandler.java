4263594b4ba4ee07094ed4326a9ed7468ea05543
pawan-kavinda <138522618+pawan-kavinda@users.noreply.github.com>
2024-05-12 11:44:02 +0530

    final

--- java/src/clientHandler.java
+++ java/src/clientHandler.java
@@ -6,51 +6,45 @@ import java.net.Socket;
 import java.util.ArrayList;
 
 public class clientHandler implements Runnable {
-		String clientmsg;
-        private Socket client;
-        private BufferedReader input;
-        private BufferedReader in;
-        private PrintWriter out;
-        private  ArrayList<clientHandler> clientArray ;
+    String clientmsg;
+    private Socket client;
+    private BufferedReader input;
+    private BufferedReader in;
+    private PrintWriter out;
+    private ArrayList<clientHandler> clientArray;
 
-        public clientHandler(Socket socket, ArrayList<clientHandler> clientArray) {           
+    public clientHandler(Socket socket, ArrayList<clientHandler> clientArray) {
+        try {
+            this.client = socket;
+            this.clientArray = clientArray;
+            input = new BufferedReader(new InputStreamReader(System.in));
+            out = new PrintWriter(client.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            clientmsg = "";
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        while (true) {
             try {
-                this.client = socket;
-                this.clientArray = clientArray;
-                input = new BufferedReader(new InputStreamReader(System.in));
-                out =new PrintWriter(client.getOutputStream(),true);
-                in= new BufferedReader(new InputStreamReader(client.getInputStream()));
-                clientmsg="";
+                clientmsg = in.readLine();
 
+                outToAll(clientmsg);
             } catch (IOException e) {
-                
+
                 e.printStackTrace();
             }
-        }        
-        
-        @Override
-        public void run(){
-            while(true) {			
-                try {
-                    clientmsg = in.readLine();
-                    if(clientmsg.equals("exit")) {
-                        out.println("GOOD BYE");
-                        in.close();
-                        out.close();
-                        input.close();
-                        client.close();                            
-                    }
-            outToAll(clientmsg);            
-        }	
-                catch (IOException e) {
-                    
-                    e.printStackTrace();
-                }                     
-        }         
-        
+        }
+
     }
-    public void outToAll(String clientmsg){
-        for(clientHandler aclient:clientArray ){
+
+    public void outToAll(String clientmsg) {
+        for (clientHandler aclient : clientArray) {
             aclient.out.println(clientmsg);
         }
     }
--- java/src/server.java
+++ java/src/server.java
@@ -6,22 +6,20 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class server {
-	private static ExecutorService pool = Executors.newFixedThreadPool(4);				
+	private static ExecutorService pool = Executors.newFixedThreadPool(4);
 	private static ArrayList<clientHandler> clientHandlers = new ArrayList<>();
+
 	public static void main(String[] args) throws IOException {
 		System.out.println("waiting for clients");
-		ServerSocket ss = new ServerSocket(8000);		
-		
-		while(true) {			
-			Socket soc = new Socket();			
-			soc = ss.accept();			
+		ServerSocket ss = new ServerSocket(8000);
+
+		while (true) {
+			Socket soc = new Socket();
+			soc = ss.accept();
 			System.out.println("Client Connected");
 			clientHandler clientThread = new clientHandler(soc, clientHandlers);
 			clientHandlers.add(clientThread);
-			pool.execute(clientThread);			
-			
-		}		
+			pool.execute(clientThread);
+		}
 	}
-		
-	
 }
